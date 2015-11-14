4a3715dfc3aaf975d5008dc414b79a8738e6b027
nitishch <nitishchandrachinta@gmail.com>
2015-11-14 11:50:43 +0530

    This is significantly faster. Keep both copies. Original and this one too. This one is on the branch 'knnsearch' and the original one is on 'master'

--- main.m
+++ main.m
@@ -18,29 +18,64 @@ function [output_image] = main(input_image_path, orows, ocols, epsilon, window_s
     output_image = double(zeros(orows, ocols));
     output_image(1:3, 1:3) = input_image(top_left_r:top_left_r + 2, top_left_c:top_left_c+2);
 
+    pad_length = (window_size - 1)/2;
+
+    padded_output = double(zeros(size(output_image) + window_size - 1));
+    padded_output(pad_length + 1: size(output_image, 1) + pad_length,  pad_length + 1: size(output_image, 2) + pad_length) = output_image;
 
    
+    %% Collecting all windows
+    windows = [];
+    nlfilter(input_image, [window_size, window_size], @add_windows);
+    function [o] = add_windows(current_window)
+        windows = [windows;current_window(:)'];
+        o = 1;
+    end
+    
+    function [distance_vector] = distfun(z1byn, zmbyn)
+        gaussian = fspecial('gaussian', window_size, sqrt(window_size));
+        gaussian = gaussian(:)';
+        distance_vector = [];
+        for row = 1:size(zmbyn, 1)
+            mask = (z1byn ~= 0) .* (zmbyn(row, :) ~= 0).*gaussian;
+            distance_vector = [distance_vector; rms(mask .* (zmbyn(row, :) - z1byn))/sum(mask(:))];
+        end
+    end
+
+    function [o] = giveValue(probe_image)
+        if(sum(probe_image(:)) == 0) o = 100000;
+        else
+            [idx, d] = knnsearch(windows, probe_image(:)', 'Distance', @distfun, 'K', 200);
+            idx = idx(d <= (1 + epsilon) * d(1));
+            temp = windows(idx(randi(size(idx, 2))), :);
+            o = temp(ceil((window_size * window_size)/2));
+        end
+    end
     %% Step 1 - FIll up the box
     for i = 4:window_size % i is column index here
         for j = 1:i
-           output_image(j,i) = giveValue(input_image, output_image, window_size, epsilon, j, i);
+           output_image(j,i) = giveValue(padded_output(j + pad_length - pad_length : j + pad_length + pad_length,i + pad_length - pad_length : i + pad_length + pad_length));
+            padded_output(j + pad_length, i + pad_length) = output_image(j, i);
         end
         for j = i-1:-1:1
-            output_image(i, j) = giveValue(input_image, output_image, window_size, epsilon, i, j);
+            output_image(i, j) = giveValue(padded_output(i + pad_length - pad_length : i + pad_length + pad_length,j + pad_length - pad_length : j + pad_length + pad_length));
+        padded_output(i + pad_length, j + pad_length) = output_image(i, j);
         end
     end
     disp('out of first');
     %% STEP 2 - The first window_size row
     for i = window_size + 1 : ocols % i is the column index here
         for j = 1:window_size
-            output_image(j, i) = giveValue(input_image, output_image, window_size, epsilon, j , i);
+            output_image(j, i) = giveValue(padded_output(j + pad_length - pad_length : j + pad_length + pad_length,i + pad_length - pad_length : i + pad_length + pad_length));
+            padded_output(j + pad_length, i + pad_length) = output_image(j, i);
         end
     end
     disp('out of second');
     %% STEP 3
     for i = window_size + 1: orows
         for j = 1:ocols
-            output_image(i, j) = giveValue(input_image, output_image, window_size, epsilon, i, j);
+            output_image(i, j) = giveValue(padded_output(i + pad_length - pad_length : i + pad_length + pad_length,j + pad_length - pad_length : j + pad_length + pad_length));
+            padded_output(i + pad_length, j + pad_length) = output_image(i, j);
         end
         i
     end
@@ -48,32 +83,20 @@ end
 
 
 
-function [outputValue] = giveValue(input_image, output_image, windowSize, epsilon, row, col)
- %% Output image padding
-    pad_length = (windowSize - 1)/2;
-    padded_output = double(zeros(size(output_image) + windowSize - 1));
-    padded_output(pad_length + 1: size(output_image, 1) + pad_length,  pad_length + 1: size(output_image, 2) + pad_length) = output_image;
-    %output_image  = padded_output;
-    
-    probe_image = padded_output(row + pad_length - pad_length:row + pad_length + pad_length, col + pad_length - pad_length:col + pad_length + pad_length);
-
-    padLength = (windowSize - 1)/2;
-    errorImage = nlfilter(input_image, [windowSize, windowSize], @intermediate);
-    function [o] = intermediate(given_image)
-        mask = (probe_image ~= 0) .* (given_image ~= 0);
-        temp_matrix = (probe_image -  given_image) .* mask;
-        o = rms(temp_matrix(:))/sum(mask(:));
-    end
-    %h = findobj(allchild(0), 'type', 'figure', 'tag', 'TMWWaitbar');
-    %set(h, 'visibility', 'off')
-    closestOnes = input_image .* (errorImage <= (1 + epsilon) * min(errorImage(:)));
-    closestOnes = nonzeros(closestOnes);
-    %closestOnes
-    %size(closestOnes)
-    outputValue = 0;
-%    for i = 1:100
- %       outputValue = outputValue + closestOnes(randi(size(closestOnes, 1)));
-%    end
-%    outputValue = outputValue/100;
-    outputValue = closestOnes(randi(size(closestOnes, 1)));
-end
+% function [outputValue] = giveValue(input_image, probe_image, windowSize, epsilon)
+%  %% Output image padding
+%     errorImage = nlfilter(input_image, [windowSize, windowSize], @intermediate);
+%     function [o] = intermediate(given_image)
+%         mask = (probe_image ~= 0) .* (given_image ~= 0) .* fspecial('gaussian', windowSize, sqrt(windowSize));
+%         temp_matrix = (probe_image -  given_image) .* mask;
+%         if sum(mask(:)) == 0
+%             o = 100000;
+%         else
+%             o = rms(temp_matrix(:))/sum(mask(:));
+%         end
+%     end
+%     closestOnes = input_image .* (errorImage <= (1 + epsilon) * min(errorImage(:)));
+%     closestOnes = nonzeros(closestOnes);
+%     outputValue = 0;
+%     outputValue = closestOnes(randi(size(closestOnes, 1)));
+% end
